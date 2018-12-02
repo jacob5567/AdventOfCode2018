72e7adf63ad84f5525a023c823c4ebee35c3cfac
Jacob Faulk <jacob5567@gmail.com>
2018-12-02 02:19:02 -0800

    finalized day 1 challenge

--- day1.py
+++ day1.py
@@ -1,10 +1,12 @@
 # Jacob Faulk
 
+
 def main():
     input_file = open("input/day1input.txt", "r")
     input_list = input_file.readlines()
     input_file.close()
     print(find_duplicate_frequency(input_list))
+
 
 def find_duplicate_frequency(input_list):
     frequency = 0
